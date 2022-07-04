e7db3167e9307d936e04a8795414e2a0bc971ad2
Victor Luna <vht.luna@gmail.com>
2022-07-04 12:05:18 -0300

    models

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Na base de dados serão criadas as tabalas contatos_contato
+# e contatos_categoria
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):
+    """
+    nome = tipo de campo(tamanho do texto do campo)
+    Sobrenome e email são opcionais, se ficar em branco
+    ainda será aceito
+    """
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    # Aqui ele referencia a classe acima. Se o contato for apagado,
+    # não será excluido as informações desta classe
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
