3d08a5c5c1f66d3debc37de0287ef58661fa0fc1
Victor Luna <vht.luna@gmail.com>
2022-07-05 11:55:16 -0300

    aprimoramento

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     p = Paginator(contatos, 5)
 
     page = request.GET.get('p')
@@ -17,6 +20,10 @@ def index(request):
 
 def detalhes(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/detalhes.html', {
         'contato': contato
     })
