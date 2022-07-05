2004d22345e7cbfd510c07ae551ed2e6e60374ae
Victor Luna <vht.luna@gmail.com>
2022-07-05 11:31:05 -0300

    nova funcionalidade paginacao

--- contatos/views.py
+++ contatos/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.core.paginator import Paginator
 
 
 def index(request):
     contatos = Contato.objects.all()
+    p = Paginator(contatos, 5)
+
+    page = request.GET.get('p')
+    contatos = p.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
 
 
 def detalhes(request, contato_id):
-    # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/detalhes.html', {
         'contato': contato
